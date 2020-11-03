805724ef8277b391862b62b31c50ca745d0bd0a2
Damon Garcia Garcia <damon99@hotmail.es>
2020-11-03 18:11:15 +0000

    Modificada página principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,16 @@
 @section('title', "Página principal")
 
 @section('content')
-    <h1>Página principal</h1>
+    <div class="row">
+        <div class="col">
+            <h1>Página principal</h1>
+        </div>
+    </div>
+  <div class="row">
+    <div class="col">
+        <h3>¡Bienvenido, visitante!</h3>
+        <a href="{{ url('/usuarios') }}" class="btn btn-outline-primary">Ir a Listado de usuarios</a>
+    </div>
+  </div>
             
 @endsection
