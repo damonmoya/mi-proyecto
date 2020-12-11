8536ee420c4fff591b9bd20f02ff8a6280d1a050
Damon Garcia Garcia <damon99@hotmail.es>
2020-12-11 18:26:07 +0000

    Logs de registros

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace App\Console;
 
+use App\Models\Company;
+use App\Models\Department;
+use App\Models\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +29,36 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Log::channel('records')->debug("Eliminando usuarios borrados...");
+            $deleted_users = User::onlyTrashed()->get();
+            foreach ($deleted_users as $user){
+                $msg = 'Usuario borrado "'.$user->name.'" está siendo eliminado...';
+                Log::channel('records')->debug($msg);
+                $user->forceDelete();
+            }
+            Log::channel('records')->debug("Eliminando empresas borradas...");
+            $deleted_companies = Company::onlyTrashed()->get();
+            foreach ($deleted_companies as $company){
+                $msg = 'Empresa borrada "'.$company->name.'" está siendo eliminada...';
+                Log::channel('records')->debug($msg);
+                $company->forceDelete();
+            }
+            Log::channel('records')->debug("Eliminando departamentos borrados...");
+            $deleted_departments = Department::onlyTrashed()->get();
+            foreach ($deleted_departments as $department){
+                $msg = 'Departamento borrado "'.$department->name.'" está siendo eliminado...';
+                Log::channel('records')->debug($msg);
+                $department->forceDelete();
+            }
+
+            $users = User::all()->count();
+            $companies = Company::all()->count();
+            $departments = Department::all()->count();
+            Log::channel('records')->debug("Usuarios registrados: {$users}");
+            Log::channel('records')->debug("Empresas registradas: {$companies}");
+            Log::channel('records')->debug("Departamentos registrados: {$departments}");
+        })->daily()->fridays();
     }
 
     /**
--- config/logging.php
+++ config/logging.php
@@ -99,6 +99,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'records' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/records.log'),
+            'level' => 'debug',
+        ],
     ],
 
 ];
